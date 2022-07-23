cfdb31065e14bccefeb36e7f8191e8ade74aee1d
Fani2000 <42602674+Fani2000@users.noreply.github.com>
2022-07-23 23:08:00 +0200

    Created, added and removed element from the DOM tree

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,4 +31,17 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+// CREATING ELEMENTS
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+const message_content =
+  'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  message_content + '<button class="btn btn--close-cookie">Got it!</button>';
+header.append(message);
+
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  message.remove();
 });
