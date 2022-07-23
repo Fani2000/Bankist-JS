534399da5d95b978ebf0987223ed4e5ba738b8e1
Fani2000 <42602674+Fani2000@users.noreply.github.com>
2022-07-23 23:34:22 +0200

    Added scrolling to the section from the learn more button

--- script.js
+++ script.js
@@ -6,6 +6,8 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const header = document.querySelector('.header');
 
@@ -40,8 +42,34 @@ const message_content =
   'We use cookies for improved functionality and analytics.';
 message.innerHTML =
   message_content + '<button class="btn btn--close-cookie">Got it!</button>';
+
+// STYLES
+message.style.backgroundColor = '#37383d';
+message.style.width = '90%';
+
 header.append(message);
 
 document.querySelector('.btn--close-cookie').addEventListener('click', () => {
   message.remove();
+});
+
+// console.log(
+//   document.documentElement.style.setProperty('--color-primary', 'orange')
+// );
+
+// Scrolling to the section
+// console.log(btnScrollTo);
+// console.log(section1);
+
+btnScrollTo.addEventListener('click', e => {
+  const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // });
+
+  // New Way Of Implementing scrollTo
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
