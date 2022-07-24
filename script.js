b9418b2da6573464bf8a67e12e70c2430bbf9f31
Fani2000 <42602674+Fani2000@users.noreply.github.com>
2022-07-24 02:41:09 +0200

    Added lazyLoading for images using IntersectingObserver API

--- script.js
+++ script.js
@@ -214,7 +214,7 @@ const allSections = document.querySelectorAll('.section');
 
 const revealSection = (entries, observer) => {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
 
   if (!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden');
@@ -235,3 +235,29 @@ allSections.forEach(section => {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// FEATURES IMGS LAZY LOADING
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = (entries, observer) => {
+  const [entry] = entries;
+  console.log(entries);
+  if (!entry.isIntersecting) return;
+
+  // Replace the src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', () => {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
